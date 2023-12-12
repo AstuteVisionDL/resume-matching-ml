3107a45dc434d8d84abda09df73b865c3d6b22ef
arkadiysotnikov <cotnikoarkady@gmail.com>
2023-12-12 20:05:01 +0400

    feat(mlpipeline): job title embeddings

--- src/job_match_ml/pipelines/data_science/nodes.py
+++ src/job_match_ml/pipelines/data_science/nodes.py
@@ -2,3 +2,28 @@
 This is a boilerplate pipeline 'data_science'
 generated using Kedro 0.18.14
 """
+import pandas as pd
+import wget
+from navec import Navec
+import os
+import numpy as np
+
+
+def extract_job_title_resumes_embeddings(processed_resumes: pd.DataFrame) -> pd.DataFrame:
+    """
+    Extracts job title from resumes and generates embeddings for them.
+    """
+    if not os.path.exists("navec_hudlit_v1_12B_500K_300d_100q.tar"):
+        wget.download("https://storage.yandexcloud.net/natasha-navec/packs/navec_hudlit_v1_12B_500K_300d_100q.tar")
+    model = Navec.load("navec_hudlit_v1_12B_500K_300d_100q.tar")
+
+    def extract_job_title_embeddings(row):
+        job_title = row["job"].split(" ")
+        job_title = [word for word in job_title if word in model]
+        if len(job_title) == 0:
+            return np.zeros(300)
+        job_embeddings = np.array([model[word] for word in job_title])
+        return np.mean(job_embeddings, axis=0)
+
+    processed_resumes["job_embeddings"] = processed_resumes.apply(extract_job_title_embeddings, axis=1)
+    return processed_resumes
--- src/job_match_ml/pipelines/data_science/pipeline.py
+++ src/job_match_ml/pipelines/data_science/pipeline.py
@@ -3,8 +3,16 @@ This is a boilerplate pipeline 'data_science'
 generated using Kedro 0.18.14
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, pipeline, node
+from .nodes import extract_job_title_resumes_embeddings
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            extract_job_title_resumes_embeddings,
+            inputs="processed_resumes",
+            outputs="resumes_embeddings",
+            name="extract_job_title_resumes_embeddings",
+        )
+    ])
